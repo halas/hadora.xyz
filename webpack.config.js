918abc20046544ba4731e4cf941b27c311b95a77
Piotr Halasiewicz <piotr.halasiewicz@syzygy.pl>
2016-11-11 02:39:35 +0100

    Adds postcss autoprefixer and cssnano

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
 
 module.exports = {
   // webpack folder's entry js - excluded from jekll's build process.
-  entry: "./webpack/entry.js",
+  entry: {
+      "src/assets/js": "./webpack/entry.js",
+      "public/assets/css": "./sass/main.scss"
+  },
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
-      path: 'src/assets/js/',
-      filename: "bundle.js"
+      path: './',
+      filename: "src/assets/js/bundle.js"
   },
   devtool: 'source-map',
   module: {
@@ -22,16 +28,18 @@ module.exports = {
       {
          test: /\.scss$/,
          loader: ExtractTextPlugin.extract(
-                "style",
-                "css-loader!sass-loader"
+                'style',
+                'css!postcss!sass'
               ),
        },
     ]
   },
+  postcss() {
+      return [autoprefixer,cssnano];
+  },
   plugins: [
-    new ExtractTextPlugin('css/main.css', {
+    new ExtractTextPlugin('public/assets/css/main.css', {
       allChunks: true,
-      path: 'public/assets/' 
     })
   ]
 };
