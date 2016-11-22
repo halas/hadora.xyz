b03bd3093adc56f2776cef10f38a8b45e4094146
Piotr Halasiewicz <piotr.halasiewicz@syzygy.pl>
2016-11-22 22:14:42 +0100

    Fix double execution of decorate()

--- webpack/entry.js
+++ webpack/entry.js
@@ -2,6 +2,8 @@ import instantClick from 'instantclick'
 let decorate = require('./components/decorate');
 
 instantClick.init()
+
+// initial decorate on hp load 
 decorate()
 instantClick.on('change', () => {
   decorate()
